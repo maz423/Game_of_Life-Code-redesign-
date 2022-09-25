89e61bb802c9941866a6f72785cef81d8adf5fa3
Mohammad Zargar <maz423@usask.ca>
2022-09-25 11:23:56 -0600

    Added function to find neighbours

--- GoL/src/main.rs
+++ GoL/src/main.rs
@@ -29,8 +29,6 @@ fn Generate_grid(data:String,rows:i32,cols:i32) -> Vec<Vec<i32>>{
 
     }
     
-    
-
    let mut state: Vec<Vec<i32>> = Vec::new();
    let mut index = 0;
    for _i in 0..rows {
@@ -51,14 +49,66 @@ fn Generate_grid(data:String,rows:i32,cols:i32) -> Vec<Vec<i32>>{
       }
       state.push(Rvec);
 
-
-    
-    
-    
-    
-
-   };
+};
 
    state
 
+}
+
+
+fn Find_neighbours(state: &Vec<Vec<i32>>,index:(i32,i32)) -> Vec<(i32,i32)> {
+    let row_len = state.len() as i32;
+    let col_len = state[0].len() as i32;
+    let mut Index_nbr:Vec<(i32,i32)> = Vec::new();
+    let mut row = index.0;
+    let mut col = index.1;
+    
+    //right cell
+    if col + 1 < col_len {
+       let indx = (row, col + 1);
+       Index_nbr.push(indx);
+ 
+    }
+    //left cell
+     if col - 1 >= 0 {
+     let indx = (row, col - 1);
+     Index_nbr.push(indx);
+ 
+    }
+    //bottom cell
+     if row - 1 >= 0 {
+     let indx = (row - 1, col);
+     Index_nbr.push(indx);}
+    
+    //Top cell
+     if row + 1 < row_len {
+     let indx = (row + 1, col);
+     Index_nbr.push(indx);}
+ 
+ 
+    //Top right
+     if row + 1 < row_len && col + 1 < col_len {
+     let indx = (row + 1, col + 1);
+     Index_nbr.push(indx);}
+ 
+ 
+    //Top Left
+     if row + 1 < row_len && col - 1 >= 0 {
+     let indx = (row + 1, col - 1);
+     Index_nbr.push(indx);}
+ 
+    //bottom right
+     if row - 1 >= 0 && col + 1 < col_len{
+     let indx = (row - 1, col + 1);
+     Index_nbr.push(indx);}
+ 
+    //bottom left
+     if row - 1 >= 0 && col - 1 >= 0 {
+     let indx = (row - 1, col - 1);
+     Index_nbr.push(indx);}
+ 
+ 
+ Index_nbr
+    
+ 
 }
